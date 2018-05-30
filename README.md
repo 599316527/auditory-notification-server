d0bd23171c90be166ae133d04fa429e93beb29f4
hekai02 <hekai02@baidu.com>
2018-05-30 23:52:12 +0800

    Support custom player

--- README.md
+++ README.md
@@ -10,3 +10,11 @@ npm run start
 curl -X POST http://localhost:3000/play \
   --data-urlencode name=Super_Mario_Level_Complete
 ```
+
+**For Windows User**
+
+`cmdmp3` is tested by me on Windows Server 2016 to play audio from command line. See details about it in this [blog](https://lawlessguy.wordpress.com/2015/06/27/update-to-a-command-line-mp3-player-for-windows/). Passing it with parameter `player` <small>(alias `p`)</small> to the server may solve the issue. 
+
+```
+npm run start -- -p "path\to\cmdmp3"
+```
--- index.js
+++ index.js
@@ -5,10 +5,19 @@ const express = require('express')
 const playSound = require('play-sound')
 const bodyParser = require('body-parser')
 
+const yargs = require('yargs')
+const argv = yargs.option('player', {alias: 'p'}).argv
+
 const app = express()
 const port = process.env.PORT || 3000
 const audioPath = path.join(__dirname, 'audio')
-const player = playSound({})
+const player = playSound({
+    player: argv.player
+})
+
+if (argv.player) {
+    console.log(`[INFO]\tPlayer is set to "${argv.player}" manually`);
+}
 
 app.use(bodyParser.json({extended: false}))
 app.use(bodyParser.urlencoded({extended: false}))
@@ -16,6 +25,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.post('/play', async (req, res) => {
     let {name, type} = req.body
     if (!(await isAudioExist(name, type))) {
+        console.log(`[WARNING]\tNot found for ${name}.${type}`)
         res.status(404).end()
         return
     }
@@ -24,12 +34,13 @@ app.post('/play', async (req, res) => {
     }
     catch (err) {
         res.status(500).send(err.message).end()
+        console.log(`[ERROR]\t${err.message}`)
         return
     }
     res.status(200).end()
 })
 
-app.listen(port, () => console.log(`Server is listening on port ${port}!`))
+app.listen(port, () => console.log(`[INFO]\tServer is listening on port ${port}!`))
 
 function isAudioExist(name, type) {
     if (!name || /\.{2,}|\//.test(name)) {
